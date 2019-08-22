75db8bd84ffdd0323315f4797edeb3d5c22286b2
Eric Michaud <eric.michaud99@gmail.com>
2019-08-22 16:42:04 -0700

    reorganized

--- effective-information/no-hidden-layers/save_many.py
+++ effective-information/no-hidden-layers/save_many.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import os
@@ -13,14 +13,6 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-from bokeh.io import push_notebook, show, output_notebook, output_file
-from bokeh.plotting import figure, ColumnDataSource, save
-from bokeh.transform import linear_cmap
-from bokeh.palettes import Plasma11, Viridis11
-from bokeh.util.hex import hexbin
-from bokeh.layouts import gridplot
-output_notebook()
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -31,7 +23,7 @@ import torchvision.transforms as transforms
 from foresight.ei import ei
 
 
-# In[2]:
+# In[ ]:
 
 
 dir_path = Path().absolute()
@@ -49,7 +41,7 @@ f.close()
 print('Loaded data to variable `mnist`')
 
 
-# In[3]:
+# In[ ]:
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -58,40 +50,38 @@ dtype = torch.float32
 torch.set_default_dtype(dtype)
 
 
-# In[4]:
+# In[ ]:
 
 
-# device
+device
 
 
-# In[5]:
+# In[ ]:
 
 
 class MNISTDataset(Dataset):
     """MNIST Digits Dataset."""
     def __init__(self, data, transform=None):
-        self.mnist = data
+        """We save the dataset images as torch.tensor since saving 
+        the dataset in memory inside a `Dataset` object as a 
+        python list or a numpy array causes a multiprocessiing-related 
+        memory leak."""
+        self.images, self.labels = zip(*data)
+        self.images = torch.from_numpy(np.array(self.images)).to(dtype)
+        self.labels = torch.tensor(np.argmax(self.labels, axis=1)).to(torch.long)
         self.transform = transform
         
     def __len__(self):
-        return len(self.mnist)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        sample = self.mnist[idx]
+        image, label = self.images[idx], self.labels[idx]
         if self.transform:
-            sample = self.transform(sample)
-        return sample
-
-class ToTensor(object):
-    """Convert samples (np.ndarray, np.ndarray) to (torch.tensor, torch.tensor)"""
-    def __call__(self, sample):
-        image, one_hot_label = sample
-        image = torch.from_numpy(image).to(dtype)
-        label = torch.tensor(np.argmax(one_hot_label)).to(torch.long)
-        return (image, label)
+            image, label = self.transform((image, label))
+        return image, label
 
 
-# In[6]:
+# In[ ]:
 
 
 class SoftmaxRegression(nn.Module):
@@ -102,34 +92,21 @@ class SoftmaxRegression(nn.Module):
     
     def forward(self, x):
         return F.log_softmax(self.linear(x), dim=1)
-    
-    
-class FullyConnected(nn.Module):
-    """Single-hidden-layer dense neural network."""
-    def __init__(self, *layers):
-        super(FullyConnected, self).__init__()
-        self.layers = nn.ModuleList()
-        for i in range(len(layers) - 1):
-            self.layers.append(nn.Linear(layers[i], layers[i+1], bias=False))
-        
-    def forward(self, x):
-        for i in range(len(self.layers) - 1):
-            x = torch.tanh(self.layers[i](x))
-        return F.log_softmax(self.layers[i+1](x), dim=1)
 
 
 # ## No Hidden Layer Softmax Network
 
-# In[7]:
+# In[ ]:
 
 
-training = MNISTDataset(mnist[:60000], transform=ToTensor())
-test = MNISTDataset(mnist[60000:], transform=ToTensor())
-training_loader = torch.utils.data.DataLoader(training, batch_size=20, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test, batch_size=20, shuffle=True)
+train_data = MNISTDataset(mnist[:60000])
+test_data = MNISTDataset(mnist[60000:])
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=20, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=20, shuffle=True)
 
 
-# In[9]:
+# In[ ]:
 
 
 NUM_REALIZATIONS = 10
@@ -139,7 +116,7 @@ graph_data = []
 
 
 for realization in range(NUM_REALIZATIONS):
-    print("Starting realization {} of {}".format(realization+1, NUM_REALIZATIONS))
+    print("Starting realization {} of {}".format(realization, NUM_REALIZATIONS))
     
     model = SoftmaxRegression(28*28, 10).to(device)
     loss_fn = nn.NLLLoss()
@@ -150,32 +127,6 @@ for realization in range(NUM_REALIZATIONS):
     eis_data = []
     losses_data = [] #[(train, test), (train, test), ...]
     accuracies_data = []
-
-
-    # --- DEFINE BOKEH OUTPUT ---
-
-    # source = ColumnDataSource(data={
-    #     'num_batches': [],
-    #     'training_loss': [],
-    #     'test_loss': [],
-    #     'EI': []
-    # })
-
-    # loss_plot = figure(plot_width=800, plot_height=150, 
-    #                        x_axis_label='batches', title='Loss')
-    # EI_plot = figure(plot_width=800, plot_height=150,
-    #                 x_axis_label='batches', y_axis_label='bits', title='Effective Information')
-    # # weight_image = figure(plot_width=5*150, plot_height=2*150, title='Output Weights')
-    # # weight_image.xgrid.visible = False
-    # # weight_image.ygrid.visible = False
-
-    # loss_plot.line('num_batches', 'test_loss', source=source, line_width=2, color='red')
-    # loss_plot.line('num_batches', 'training_loss', source=source, line_width=2, color='green')
-    # EI_plot.line('num_batches', 'EI', source=source, line_width=2)
-    # # weight_image.image([np.zeros((784, 10)).reshape(112, -1)], 0, 0, 1, 1, palette=Plasma11)
-
-    # grid = gridplot([[loss_plot], [EI_plot]])
-    # display = show(grid, notebook_handle=True)
 
     def update_metrics():
         with torch.no_grad():
@@ -195,7 +146,7 @@ for realization in range(NUM_REALIZATIONS):
         outof = 0
         loss = 0
         with torch.no_grad():
-            for x, labels in islice(training_loader, 0, 500): # 500 batches of 20 samples
+            for x, labels in islice(train_loader, 0, 500): # 500 batches of 20 samples
                 output = model(x.to(device))
                 loss += loss_fn(output, labels.to(device)).item()
                 _, pred = torch.max(output, 1)
@@ -212,19 +163,6 @@ for realization in range(NUM_REALIZATIONS):
                 outof += len(labels)
         accuracy = accuracy / outof
 
-    #     source.stream({
-    #         'num_batches': [num_batches],
-    #         'training_loss': [training_loss],
-    #         'test_loss': [test_loss],
-    #         'EI': [EI]
-    #     })
-    #     weight = model.linear.weight.cpu().detach().numpy()
-    #     for i in range(len(weight)):
-    #         x = i % 5
-    #         y = i // 5
-    #         weight_image.image([weight[i].reshape(28, 28)], x*29, -y*29, 28, 28, palette=Viridis11)
-        # push_notebook(handle=display)
-
         num_batches_data.append(num_batches)
         eis_data.append(EI)
         losses_data.append((training_loss, test_loss))
@@ -237,7 +175,7 @@ for realization in range(NUM_REALIZATIONS):
 
     num_batches = 0
     for epoch in range(80):
-        for sample, target in training_loader:
+        for sample, target in train_loader:
             optimizer.zero_grad()
             loss = loss_fn(model(sample.to(device)), target.to(device))
             loss.backward()
@@ -249,11 +187,6 @@ for realization in range(NUM_REALIZATIONS):
     graph_data.append((num_batches_data, eis_data, losses_data, accuracies_data))
     
 import pickle
-with open("plots/plot0/graph_data_many.pkl", "wb") as f:
+with open("plots/graph_data_many.pkl", "wb") as f:
     pickle.dump(graph_data, f)
-
-
-
-
-
 
